3d45612bec0562333e1a09d326549391dcfb0ef0
gemece <30463151+gemece@users.noreply.github.com>
2019-08-13 23:44:06 +0200

    reduce quote

--- canciones.py
+++ canciones.py
@@ -115,7 +115,8 @@ def api_youtube_search(options):
 	search_response = youtube.search().list(
 	  q=options,
 	  part="id,snippet",
-	  #maxResults=options.max_results,
+	  maxResults=1,
+	  fields="items/id",
 	  order="viewCount"
 	).execute()
 	#f = open("kk.txt","w")
